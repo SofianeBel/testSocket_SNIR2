49476814313f6951a2a2dbe569a5bb66bad0f093
Sifly <118746369+SofianeBel@users.noreply.github.com>
2024-03-14 10:41:01 +0100

    fix style

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Socket w;
+
+    QFile file("path/StyleExample.qss");
+    file.open(QFile::ReadOnly);
+
+    QString styleSheet{ QLatinlString(file.readAll()) };
+
+    //setup stylesheet
+    app.setStyleSheet(styleSheet);
+
     w.show();
 
     return a.exec();
